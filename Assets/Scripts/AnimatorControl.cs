7a9066849ac45ddcfc435520a129a282424acc63
finaltracker <amoi.jinxp@gmail.com>
2016-12-23 18:12:18 +0800

    Add Animation

---
+++ Assets/Scripts/AnimatorControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimatorControl : MonoBehaviour, IPointerClickHandler {
+	Animator anima;
+	// Use this for initialization
+	void Start () {
+		anima = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnPointerClick(PointerEventData data)
+	{
+		#if UNITY_EDITOR
+		Debug.Log ("squiz trigger");
+		#endif
+		anima.SetTrigger ("squizTrigger");
+	}
+		
+}
