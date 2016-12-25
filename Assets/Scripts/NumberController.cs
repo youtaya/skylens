ecf8a6251822750271537e33a97da49011210d38
youtaya <jxiaoping@gmail.com>
2016-12-25 12:00:20 +0800

    Shrink the code

--- Assets/Scripts/NumberController.cs
+++ Assets/Scripts/NumberController.cs
@@ -12,15 +12,42 @@ public class NumberController : MonoBehaviour {
 	public Text roundText;
 	public Text notificationText;
 
-	static TouchNumber[] groupNums = new TouchNumber[9];
+	SortedDictionary<string, int> numDict = new SortedDictionary<string, int> ();
+	SortedDictionary<string, int> padDict = new SortedDictionary<string, int> ();
+
+	List<TouchNumber> listNum = new List<TouchNumber> ();
 	static int groupIndex = 0;
 	static bool isCameraRotate = false;
+	static bool isRevertCond = false;
+
+	#if (UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN)
+	[DllImport ("flick")]
+	private static extern void magicAllIn(byte[] sb, int v1, int v2, int v3);
+	#elif (UNITY_EDITOR_OSX || UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX)
+	[DllImport ("flick")]
+	private static extern IntPtr magicAllIn(int v1, int v2, int v3);
+	#endif
 
 	// Use this for initialization
 	void Start () {
 		#if UNITY_EDITOR
 		Debug.Log ("number controller");
 		#endif
+		numDict.Add ("One", 1);
+		numDict.Add ("Two", 2);
+		numDict.Add ("Three", 3);
+		numDict.Add ("Four", 4);
+		numDict.Add ("Five", 5);
+		numDict.Add ("Six", 6);
+		numDict.Add ("Seven", 7);
+		numDict.Add ("Eight", 8);
+		numDict.Add ("Nine", 9);
+		numDict.Add ("Zero", 0);
+
+		padDict.Add ("Touch Pad 1", 1);
+		padDict.Add ("Touch Pad 2", 2);
+		padDict.Add ("Touch Pad 3", 3);
+
 	}
 	
 	// Update is called once per frame
@@ -31,35 +58,70 @@ public class NumberController : MonoBehaviour {
 	void LateUpdate() {
 		ShowRoundText ();
 		if (groupIndex > 0 && (groupIndex % 3 == 0)) {
-			if (!isCameraRotate) {
+			if (!isCameraRotate && !isRevertCond) {
 				Camera.main.transform.Rotate (new Vector3 (0, 90, 0));
 			}
 			isCameraRotate = true;
 		} else {
 			isCameraRotate = false;
 		}
+
+		if (groupIndex == 9) {
+			
+			#if (UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN)
+			byte[] sb = new byte[256];
+			magicAllIn (sb, 200, 234, 345);
+			string screenStr="";
+			screenStr=System.Text.Encoding.Default.GetString(sb);
+			Debug.Log ("sb result:  " + screenStr);
+			notificationText.text = screenStr;
+			#elif (UNITY_EDITOR_OSX || UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX)
+			IntPtr pStr = magicAllIn (200, 234, 345);
+			string res = Marshal.PtrToStringAnsi (pStr);
+			//Marshal.FreeHGlobal (pStr);
+			notificationText.text = res;
+			#endif
+
+		}
 	}
 
 	void ShowRoundText () {
 		roundText.text = "Round ";
 		string temp = "";
-		for (int i = 0; i < 9; i++) {
-			if (groupNums [i] != null) {
-				temp += " " + groupNums [i].number;
+		for (int i = 0; i < listNum.Count; i++) {
+			if (listNum[i] != null) {
+				temp += " " + listNum [i].number;
 			}
 		}
 		roundText.text += temp;
 	}
 
-	bool hasTouched(int n, int pad) {
-		for (int i = 0; i < 9; i++) {
-			if (groupNums [i] != null) {
-				if (groupNums [i].number == n && groupNums [i].belongPad == pad) {
-					return true;
+	TouchNumber getTouchedNum(int n, int pad) {
+		for (int i = 0; i < listNum.Count; i++) {
+			if (listNum [i] != null) {
+				if (listNum [i].number == n && listNum [i].belongPad == pad) {
+					return listNum [i];
 				}
 			}
 		}
-		return false;
+		return null;
+	}
+
+	void recordNumber(int num, int pad) {
+
+		TouchNumber touched = getTouchedNum (num, pad);
+		if(touched != null) {
+			Debug.Log ("has touched!");
+			listNum.Remove (touched);
+			groupIndex -= 1;
+			isRevertCond = true;
+
+		} else {
+			TouchNumber tn = new TouchNumber (num, pad);
+			listNum.Add (tn);
+			groupIndex++;
+			isRevertCond = false;
+		}
 	}
 
 	public void ClickNumberEvent(BaseEventData data) {
@@ -68,126 +130,17 @@ public class NumberController : MonoBehaviour {
 		Debug.Log ("hit object: "+temp.pointerCurrentRaycast);
 		Debug.Log ("parent of hit object: "+temp.pointerPressRaycast.gameObject.transform.parent.gameObject);
 		#endif
-		if (temp.pointerCurrentRaycast.gameObject.name == "One") {
-			int touchPad = 1;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				touchPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				touchPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				touchPad = 3;
-			}
-			if(hasTouched(1, touchPad)) {
-				Debug.Log ("has touched!");
-				
-			} else {
-				TouchNumber tn = new TouchNumber (1, touchPad, groupIndex);
-				groupNums [groupIndex] = tn;
-				groupIndex++;
-			}
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Two") {
-			groupNums [groupIndex].number = 2;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				groupNums [groupIndex].belongPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				groupNums [groupIndex].belongPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				groupNums [groupIndex].belongPad = 3;
-			}
-			groupNums [groupIndex].index = groupIndex;
-			groupIndex++;
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Three") {
-			groupNums [groupIndex].number = 3;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				groupNums [groupIndex].belongPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				groupNums [groupIndex].belongPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				groupNums [groupIndex].belongPad = 3;
-			} 
-			groupNums [groupIndex].index = groupIndex;
-			groupIndex++;
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Four") {
-			groupNums [groupIndex].number = 4;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				groupNums [groupIndex].belongPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				groupNums [groupIndex].belongPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				groupNums [groupIndex].belongPad = 3;
-			} 
-			groupNums [groupIndex].index = groupIndex;
-			groupIndex++;
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Five") {
-			groupNums [groupIndex].number = 5;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				groupNums [groupIndex].belongPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				groupNums [groupIndex].belongPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				groupNums [groupIndex].belongPad = 3;
-			} 
-			groupNums [groupIndex].index = groupIndex;
-			groupIndex++;
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Six") {
-			groupNums [groupIndex].number = 6;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				groupNums [groupIndex].belongPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				groupNums [groupIndex].belongPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				groupNums [groupIndex].belongPad = 3;
-			} 
-			groupNums [groupIndex].index = groupIndex;
-			groupIndex++;
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Seven") {
-			groupNums [groupIndex].number = 7;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				groupNums [groupIndex].belongPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				groupNums [groupIndex].belongPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				groupNums [groupIndex].belongPad = 3;
-			} 
-			groupNums [groupIndex].index = groupIndex;
-			groupIndex++;
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Eight") {
-			groupNums [groupIndex].number = 8;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				groupNums [groupIndex].belongPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				groupNums [groupIndex].belongPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				groupNums [groupIndex].belongPad = 3;
-			} 
-			groupNums [groupIndex].index = groupIndex;
-			groupIndex++;
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Nine") {
-			groupNums [groupIndex].number = 9;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				groupNums [groupIndex].belongPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				groupNums [groupIndex].belongPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				groupNums [groupIndex].belongPad = 3;
-			} 
-			groupNums [groupIndex].index = groupIndex;
-			groupIndex++;
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Zero") {
-			groupNums [groupIndex].number = 0;
-			if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 1") {
-				groupNums [groupIndex].belongPad = 1;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 2") {
-				groupNums [groupIndex].belongPad = 2;
-			} else if (temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name == "Touch Pad 3") {
-				groupNums [groupIndex].belongPad = 3;
-			} 
-			groupNums [groupIndex].index = groupIndex;
-			groupIndex++;
-		} else if (temp.pointerCurrentRaycast.gameObject.name == "Back") {
+
+		if (temp.pointerCurrentRaycast.gameObject.name == "Back") {
 			Camera.main.transform.Rotate (new Vector3 (0, -90, 0));
 		} else if (temp.pointerCurrentRaycast.gameObject.name == "Forward") {
 			Camera.main.transform.Rotate (new Vector3 (0, 90, 0));
+		} else {
+
+			recordNumber (numDict [temp.pointerCurrentRaycast.gameObject.name], 
+				padDict [temp.pointerPressRaycast.gameObject.transform.parent.gameObject.name]);
 		}
+
+
 	}
 }
--- Assets/Scripts/TouchNumber.cs
+++ Assets/Scripts/TouchNumber.cs
@@ -6,6 +6,12 @@ public class TouchNumber
 	public int belongPad;
 	public int index;
 
+	public TouchNumber (int v1, int v2)
+	{
+		number = v1;
+		belongPad = v2;
+	}
+
 	public TouchNumber (int v1, int v2, int v3)
 	{
 		number = v1;
